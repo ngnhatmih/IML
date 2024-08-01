51d6bcdf55ea776b1dac290025aa895833397bce
ngnhatmih <ngnhatmih9997@gmail.com>
2024-08-01 23:35:41 +0700

    first hello world window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
-#include <iostream>
+#include <SDL3/SDL.h>
+
+SDL_Window *w = nullptr;
+SDL_Renderer *r = nullptr;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    SDL_Init(SDL_INIT_VIDEO || SDL_INIT_EVENTS);
+    w = SDL_CreateWindow("HelloWorld", 300, 500, 0);
+    r = SDL_CreateRenderer(w, 0);
+
+    SDL_Event e;
+    while (1) {
+        if (SDL_PollEvent(&e)) {
+            if (e.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED) {
+                break;
+            }
+        }
+    }
+
+    SDL_DestroyRenderer(r);
+    SDL_DestroyWindow(w);
+    SDL_Quit();
     return 0;
 }
