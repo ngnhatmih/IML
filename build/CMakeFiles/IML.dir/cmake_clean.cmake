9aed37b273a45f14684a2b7ee86dc620891c679c
ngnhatmih <ngnhatmih9997@gmail.com>
2024-08-01 23:02:32 +0700

    test

---
+++ build/CMakeFiles/IML.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IML.dir/src/main.cpp.obj"
+  "CMakeFiles/IML.dir/src/main.cpp.obj.d"
+  "IML.exe"
+  "IML.exe.manifest"
+  "IML.pdb"
+  "libIML.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IML.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/vendor/sdl/CMakeFiles/SDL3_test.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/ngnhatmih/Desktop/ngn/projects/imgui/IML/vendor/sdl/src/test/SDL_test_assert.c" "vendor/sdl/CMakeFiles/SDL3_test.dir/src/test/SDL_test_assert.c.obj" "gcc" "vendor/sdl/CMakeFiles/SDL3_test.dir/src/test/SDL_test_assert.c.obj.d"
+  "C:/Users/ngnhatmih/Desktop/ngn/projects/imgui/IML/vendor/sdl/src/test/SDL_test_common.c" "vendor/sdl/CMakeFiles/SDL3_test.dir/src/test/SDL_test_common.c.obj" "gcc" "vendor/sdl/CMakeFiles/SDL3_test.dir/src/test/SDL_test_common.c.obj.d"
+  "C:/Users/ngnhatmih/Desktop/ngn/projects/imgui/IML/vendor/sdl/src/test/SDL_test_compare.c" "vendor/sdl/CMakeFiles/SDL3_test.dir/src/test/SDL_test_compare.c.obj" "gcc" "vendor/sdl/CMakeFiles/SDL3_test.dir/src/test/SDL_test_compare.c.obj.d"
+  "C:/Users/ngnhatmih/Desktop/ngn/projects/imgui/IML/vendor/sdl/src/test/SDL_test_crc32.c" "vendor/sdl/CMakeFiles/SDL3_test.dir/src/test/SDL_test_crc32.c.obj" "gcc" "vendor/sdl/CMakeFiles/SDL3_test.dir/src/test/SDL_test_crc32.c.obj.d"
+  "C:/Users/ngnhatmih/Desktop/ngn/projects/imgui/IML/vendor/sdl/src/test/SDL_test_font.c" "vendor/sdl/CMakeFiles/SDL3_test.dir/src/test/SDL_test_font.c.obj" "gcc" "vendor/sdl/CMakeFiles/SDL3_test.dir/src/test/SDL_test_font.c.obj.d"
+  "C:/Users/ngnhatmih/Desktop/ngn/projects/imgui/IML/vendor/sdl/src/test/SDL_test_fuzzer.c" "vendor/sdl/CMakeFiles/SDL3_test.dir/src/test/SDL_test_fuzzer.c.obj" "gcc" "vendor/sdl/CMakeFiles/SDL3_test.dir/src/test/SDL_test_fuzzer.c.obj.d"
+  "C:/Users/ngnhatmih/Desktop/ngn/projects/imgui/IML/vendor/sdl/src/test/SDL_test_harness.c" "vendor/sdl/CMakeFiles/SDL3_test.dir/src/test/SDL_test_harness.c.obj" "gcc" "vendor/sdl/CMakeFiles/SDL3_test.dir/src/test/SDL_test_harness.c.obj.d"
+  "C:/Users/ngnhatmih/Desktop/ngn/projects/imgui/IML/vendor/sdl/src/test/SDL_test_log.c" "vendor/sdl/CMakeFiles/SDL3_test.dir/src/test/SDL_test_log.c.obj" "gcc" "vendor/sdl/CMakeFiles/SDL3_test.dir/src/test/SDL_test_log.c.obj.d"
+  "C:/Users/ngnhatmih/Desktop/ngn/projects/imgui/IML/vendor/sdl/src/test/SDL_test_md5.c" "vendor/sdl/CMakeFiles/SDL3_test.dir/src/test/SDL_test_md5.c.obj" "gcc" "vendor/sdl/CMakeFiles/SDL3_test.dir/src/test/SDL_test_md5.c.obj.d"
+  "C:/Users/ngnhatmih/Desktop/ngn/projects/imgui/IML/vendor/sdl/src/test/SDL_test_memory.c" "vendor/sdl/CMakeFiles/SDL3_test.dir/src/test/SDL_test_memory.c.obj" "gcc" "vendor/sdl/CMakeFiles/SDL3_test.dir/src/test/SDL_test_memory.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
