5368a3efcef2381f37106f3189dff78d1fedf8ad
ngnhatmih <ngnhatmih9997@gmail.com>
2024-08-01 23:05:45 +0700

    test

---
+++ build/CMakeFiles/CMakeScratch/TryCompile-xpn0uj/CMakeFiles/cmTC_c8d15.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_c8d15.dir/CheckIncludeFile.c.obj"
+  "cmTC_c8d15.exe"
+  "cmTC_c8d15.exe.manifest"
+  "cmTC_c8d15.pdb"
+  "libcmTC_c8d15.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_c8d15.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
